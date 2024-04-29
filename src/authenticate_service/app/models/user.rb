05167a688e12394eddcb28ae1416156a9a5d282e
Rishi Rajkumar <jrishi007@gmail.com>
2024-04-29 15:57:15 +0100

    Feature CD for News Service and Auth Service (#7)
    * Fix  stuff to trigger build
    
    * Fix issue
    
    * Add Deployment Changes
    
    * Add Chaining of CI and CD in sequence
    
    ---------
    
    Co-authored-by: Rishi J <rishij@Rishis-MacBook-Air.local>

--- src/authenticate_service/app/models/user.rb
+++ src/authenticate_service/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   include Devise::JWT::RevocationStrategies::JTIMatcher
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # TODO: Later add the forgot password and other features
   devise :database_authenticatable, :registerable, :recoverable, :trackable, :lockable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 end
