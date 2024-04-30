6a10df7bc3abb496f3bd7e7e88fcea60dfcae52d
Hari <hariprasad@Haris-MacBook-Air.local>
2024-04-30 19:59:32 +0100

    Add image src per post

--- src/web-ui/src/components/AppPost.vue
+++ src/web-ui/src/components/AppPost.vue
@@ -1,5 +1,7 @@
 <template>
-  <b-card :title="post.title">
+  <b-card :title="post.title"
+  :img-src="post.banner_image"
+  >
     <span class="card-text" v-html="this.$options.filters.dataSubstr(post.summary)"></span>
     <em slot="footer" ><a  class="card-link" :href="post.url" target="_blank">Read More</a></em>
   </b-card>
