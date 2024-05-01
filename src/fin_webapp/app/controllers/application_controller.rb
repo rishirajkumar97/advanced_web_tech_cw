b67651f94a76c0a23f0f09ac83a5bfcc0641564d
Rishi J <rishij@Rishis-MacBook-Air.local>
2024-05-01 16:36:24 +0100

    add auth concern

--- src/fin_webapp/app/controllers/application_controller.rb
+++ src/fin_webapp/app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+    before_action :validate_token
+
+    private
+  
+    def validate_token
+      response = Faraday.get('http://meta-geography-243114.nw.r.appspot.com/validate_token') do |req| # Endpoint of the Main Service
+        req.headers['Authorization'] = request.headers['Authorization']
+      end
+  
+      # Check if the response status is 200 OK
+      render json: { error: 'Unauthorized' }, status: :unauthorized unless response.status == 200
+    end
 end
