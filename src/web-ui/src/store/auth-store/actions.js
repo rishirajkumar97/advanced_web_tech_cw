0fc95e970cc925e7a90dd270e12a409336a8869f
Rishi J <rishij@Rishis-MacBook-Air.local>
2024-05-03 21:06:11 +0100

    Add Action JS with proper UI tokens torage and setup

--- src/web-ui/src/store/auth-store/actions.js
+++ src/web-ui/src/store/auth-store/actions.js
@@ -5,12 +5,12 @@ export const actions = {
     loginAndSaveToken(context, { username, password }) {
         // this is just an example on how to save the JWT Token, PS AUTH API is fake
         axios.post('https://meta-geography-243114.nw.r.appspot.com/login', { user:  { email: username, password: password } } ).then((response) => {
-            console.log(response);
-            localStorage.setItem('auth_token', response.headers.get('authorization'));
+            let token = response.data.status.data.token;
+            localStorage.setItem('auth_token', token);
             router.push('/category/front-end');
         }).catch((error) => {
             console.log(error);
-            //will always redirect to profile 
+            //will always redirect to profile np
             router.push('/404');
         });
     },
