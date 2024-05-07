d24a53daefacfd9c174d9c3bd5b6c09bc1497eff
Rishi J <rishij@Rishis-MacBook-Air.local>
2024-05-07 06:47:51 +0100

    Fix issue with Stock data Service

--- src/stock_data_service/app/controllers/companies_controller.rb
+++ src/stock_data_service/app/controllers/companies_controller.rb
@@ -1,9 +1,7 @@
 class CompaniesController < ApplicationController
-    require 'httparty'
-  
     def index
-      response = HTTParty.get('https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=WXVT1MC2X617DPMV')
-      @data = response.parsed_response if response.code == 200
-      render json: @data
+      # response = HTTParty.get('https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=WXVT1MC2X617DPMV')
+      # @data = response.parsed_response if response.code == 200
+      render json:  []
     end
   end
