d05723fae41e56711727034ae40e0fff924fd114
Rishi Rajkumar <jrishi007@gmail.com>
2024-05-07 01:55:55 +0100

    Feature: Stock Service Updated (#35)
    * backend dynamic query push
    
    * Fix app.yaml
    
    * Fix Docker File
    
    * Fix Docker File
    
    * Fix Gemfile.Lock
    
    * Change Hardcode Logic
    
    ---------
    
    Co-authored-by: John Kennedy <jackakennedy@yahoo.com>
    Co-authored-by: Rishi J <rishij@Rishis-MacBook-Air.local>

--- src/stock_data_service/app/controllers/stocks_controller.rb
+++ src/stock_data_service/app/controllers/stocks_controller.rb
@@ -1,9 +1,34 @@
 class StocksController < ApplicationController
-    require 'httparty'
-  
-    def index
-      response = HTTParty.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=WXVT1MC2X617DPMV')
-      @data = response.parsed_response if response.code == 200
-      render json: @data
+  #before_action :validate_params
+
+  rescue_from ActionController::ParameterMissing do |exception|
+    render json: { error: exception.message }, status: :bad_request
+  end
+
+  def fetch_stock_data
+    alpha_vantage_service = AlphaVantageService.new
+    response_data = alpha_vantage_service.get_stock_data(params[:function], params[:symbol], params[:interval])
+
+    if response_data[:error].present?
+      render json: { error: response_data[:error] }, status: :bad_request
+    else
+      render json: response_data
     end
   end
+
+  private
+
+  #def validate_params
+  #  allowed_functions = ['TIME_SERIES_INTRADAY', 'TIME_SERIES_DAILY', 'TIME_SERIES_WEEKLY', 'TIME_SERIES_MONTHLY']
+  #  unless allowed_functions.include?(params[:function])
+  #    render json: { error: "Invalid function parameter" }, status: :bad_request
+  #  end
+
+  #  if params[:symbol].blank? || params[:interval].blank?
+  #    render json: { error: "Symbol and interval parameters cannot be blank" }, status: :bad_request
+  #  else
+  #    # Normal processing logic here
+  #    render json: { message: "Parameters are fine!" } # Example render
+  #  end
+  #end
+end
